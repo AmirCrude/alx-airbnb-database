c91cd5aa1d77159b535d0029d36f4ed4a0571bb4
Amir Abdu <amirabdu.com@gmail.com>
2025-11-02 06:37:26 +0300

    Add database index sql code

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,24 @@
+﻿
+-- ------------------------
+-- User table indexes
+-- ------------------------
+CREATE INDEX idx_user_email ON User(email);
+CREATE INDEX idx_user_role ON User(role);
+
+-- ------------------------
+-- Booking table indexes
+-- ------------------------
+CREATE INDEX idx_booking_user_id ON Booking(user_id);
+CREATE INDEX idx_booking_property_id ON Booking(property_id);
+
+-- ------------------------
+-- Property table indexes
+-- ------------------------
+CREATE INDEX idx_property_host_id ON Property(host_id);
+CREATE INDEX idx_property_location ON Property(location);
+
+-- ------------------------
+-- Payment table indexes
+-- ------------------------
+CREATE INDEX idx_payment_booking_id ON Payment(booking_id);
+
