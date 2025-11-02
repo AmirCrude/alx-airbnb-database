ac6cb54b09cc285802356046cad3fd42ab416a14
Amir Abdu <amirabdu.com@gmail.com>
2025-11-02 06:39:08 +0300

    Update index sql to have selected query for analysis

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -22,3 +22,15 @@ CREATE INDEX idx_property_location ON Property(location);
 -- ------------------------
 CREATE INDEX idx_payment_booking_id ON Payment(booking_id);
 
+-- selected query for analysis
+
+EXPLAIN ANALYZE
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    COUNT(b.booking_id) AS total_bookings
+FROM "User" u
+LEFT JOIN Booking b ON u.user_id = b.user_id
+GROUP BY u.user_id, u.first_name, u.last_name
+ORDER BY total_bookings DESC;
